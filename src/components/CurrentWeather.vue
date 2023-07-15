28b8191b9ec5583de94bf915e7e92a4f94b57f36
linnchoeuh <lennintendo@gmail.com>
2023-07-15 20:25:01 +0200

    [REFACTOR] Moving the main-info div into an SFC

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import type { CityDataResponse } from "@/stores/CityDataStore"
 import { useCityDataStore } from "@/stores/CityDataStore"
 import SideInfo from "@/components/SideInfo.vue"
+import MainInfo from "@/components/MainInfo.vue"
 
 const store = useCityDataStore();
 
@@ -19,17 +20,7 @@ function objectNotEmpty(target: object)
 
 <template>
     <div v-if="objectNotEmpty(store.currentWeather)" class="current-weather-block">
-        <div class="main-info">
-            <div class="main-info-sub">
-                <h1>Current weather</h1>
-                <img class="weather-img" :src="store.currentWeather.current.condition.icon">
-                <text class="temp"> {{ store.currentWeather.current.temp_c }}°C </text>
-                <text class="temp-feels-like"> feels like {{ store.currentWeather.current.feelslike_c }}°C </text>
-                <p></p>
-                <text class="weather-text"> {{ store.currentWeather.current.condition.text }} </text>
-                <text class="last-update-text"> Last update: {{ store.currentWeather.current.last_updated }} </text>
-            </div>
-        </div>
+        <MainInfo/>
         <div class="side-info">
             <SideInfo>
                 <template #title> UV Index </template>
@@ -68,56 +59,13 @@ function objectNotEmpty(target: object)
     }
 }
 
-.main-info {
-    padding: 15px;
-}
-
-.main-info-sub {
-    border-radius: 15px;
-    padding: 15px;
-    background-color: rgba(255, 255, 255, 0.03);
-}
-
-
 .side-info > div {
     width: 50%;
     display: inline-block;
     height: 163px;
 }
 
-.main-info h1 {
-    font-size: 50px;
-}
 
-.weather-img {
-    position: relative;
-    width: 128px;
-    top: -10px;
-}
 
-.temp {
-    position: relative;
-    top: -50px;
-    font-size: 70px;
-}
-.temp-feels-like {
-    position: relative;
-    top: -60px;
-    font-size: 30px;
-    color: #808080
-}
-
-.weather-text {
-    position: relative;
-    top: -45px;
-    left: 5px;
-    font-size: 20px;
-}
-
-.last-update-text {
-    position: relative;
-    float: right;
-    top: 10px;
-}
 
 </style>
---
+++ src/components/MainInfo.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { useCityDataStore } from "@/stores/CityDataStore"
+
+const store = useCityDataStore();
+
+</script>
+
+<template>
+
+<div class="main-info">
+    <div class="main-info-sub">
+        <h1>Current weather</h1>
+        <img class="weather-img" :src="store.currentWeather.current.condition.icon">
+        <text class="temp"> {{ store.currentWeather.current.temp_c }}°C </text>
+        <text class="temp-feels-like"> feels like {{ store.currentWeather.current.feelslike_c }}°C </text>
+        <p></p>
+        <text class="weather-text"> {{ store.currentWeather.current.condition.text }} </text>
+        <text class="last-update-text"> Last update: {{ store.currentWeather.current.last_updated }} </text>
+    </div>
+</div>
+
+</template>
+
+<style scoped>
+
+.main-info {
+    padding: 15px;
+}
+
+.main-info-sub {
+    border-radius: 15px;
+    padding: 15px;
+    background-color: rgba(255, 255, 255, 0.03);
+}
+
+.main-info h1 {
+    font-size: 50px;
+}
+
+
+.weather-img {
+    position: relative;
+    width: 128px;
+    top: -10px;
+}
+
+.temp {
+    position: relative;
+    top: -50px;
+    font-size: 70px;
+}
+.temp-feels-like {
+    position: relative;
+    top: -60px;
+    font-size: 30px;
+    color: #808080
+}
+
+.weather-text {
+    position: relative;
+    top: -45px;
+    left: 5px;
+    font-size: 20px;
+}
+
+.last-update-text {
+    position: relative;
+    float: right;
+    top: 10px;
+}
+
+@media (max-width:540px) {
+    .main-info h1 {
+        font-size: 40px;
+    }
+    .weather-img {
+        width: 64px;
+        top: -15px;
+    }
+    .temp {
+        top: -30px;
+        font-size: 40px;
+    }
+    .temp-feels-like {
+        top: -35px;
+        font-size: 20px;
+    }
+
+    .weather-text {
+        top: -30px;
+        font-size: 15px;
+    }
+
+    .last-update-text {
+        float: right;
+        top: 10px;
+    }
+}
+
+</style>
